2755f311fefa6552d5b6edea50e9942523116106
Oyku Parlak <oykuparlak2462@gmail.com>
2024-01-24 14:01:19 +0300

    latest changes

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,39 +1,44 @@
-import classes from "./event-item.module.css";
-import Button from "../ui/button";
-import DateIcon from "../icons/date-icon";
-import Address from "../icons/address-icon";
+import Image from 'next/image';
+
+import Button from '../ui/button';
+import DateIcon from '../icons/date-icon';
+import AddressIcon from '../icons/address-icon';
+import ArrowRightIcon from '../icons/arrow-right-icon';
+import classes from './event-item.module.css';
 
 function EventItem(props) {
   const { title, image, date, location, id } = props;
 
-  const readableDate = new Date(date).toLocaleDateString("en-US", {
-    day: "numeric",
-    month: "long",
-    year: "numeric",
+  const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
   });
-
-  const formattedAddress = location.replace(",", "\n");
+  const formattedAddress = location.replace(', ', '\n');
   const exploreLink = `/events/${id}`;
 
   return (
     <li className={classes.item}>
-      <img src={"/" + image} alt={title} />
-
+      <Image src={'/' + image} alt={title} width={250} height={160} />
       <div className={classes.content}>
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
             <DateIcon />
-            <time>{readableDate}</time>
+            <time>{humanReadableDate}</time>
           </div>
           <div className={classes.address}>
-            <Address />
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
-
         <div className={classes.actions}>
-          <Button link={exploreLink}>Explore Event</Button>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,34 @@
-import { getFeaturedEvents } from "../dummy-data";
-import EventList from "../components/events/event-list";
+import Head from 'next/head';
 
-function HomePage() {
-  const featuredEvents = getFeaturedEvents();
+import { getFeaturedEvents } from '../helpers/api-util';
+import EventList from '../components/events/event-list';
+import NewsletterRegistration from '../components/input/newsletter-registration';
 
-  console.log("featuredEvents:", featuredEvents);
+function HomePage(props) {
   return (
     <div>
-      <EventList items={featuredEvents} />
+      <Head>
+        <title>NextJS Events</title>
+        <meta
+          name='description'
+          content='Find a lot of great events that allow you to evolve...'
+        />
+      </Head>
+      <NewsletterRegistration />
+      <EventList items={props.events} />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: {
+      events: featuredEvents,
+    },
+    revalidate: 1800,
+  };
 }
 
 export default HomePage;
