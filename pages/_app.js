0198fcad1412c05fd84a7e4c687916b6afab8f18
Oyku Parlak <oykuparlak2462@gmail.com>
2024-01-24 17:56:07 +0300

    notification context

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,26 @@
-import Head from 'next/head';
+import Head from "next/head";
 
-import Layout from '../components/layout/layout';
-import '../styles/globals.css';
+import Layout from "../components/layout/layout";
+import Notification from "../components/ui/notification";
+import "../styles/globals.css";
+import { NotificationContextProvider } from "../store/notification-context";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Head>
-        <title>Next Events</title>
-        <meta name='description' content='NextJS Events' />
-        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-      </Head>
-      <Component {...pageProps} />
-    </Layout>
+    <NotificationContextProvider>
+      <Layout>
+        <Head>
+          <title>Next Events</title>
+          <meta name="description" content="NextJS Events" />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
+        <Component {...pageProps} />
+        <Notification title="TEST" message="THIS IS A TEST" status="pending" />
+      </Layout>
+    </NotificationContextProvider>
   );
 }
 
